2f4ff4596f2584b84fb52d3d57a60e319dee5284
devrkZone <dashrakesh179@gmail.com>
2018-07-23 00:03:07 +0530

    Initial comit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const server = express();
+hbs.registerPartials(__dirname + '/views/partials');
+server.set('view engine','hbs')
+server.use(express.static(__dirname + '/public/'))
+
+hbs.registerHelper('currentYear',()=>{
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('upperCase',(text)=>{
+  return text.toUpperCase();
+})
+
+server.get('/',(req,res)=>{
+  // res.send('<h1>Hello express</h1>');
+  res.render('home.hbs',{
+    title : 'Home',
+    currentYear : new Date().getFullYear(),
+    projectName : 'node-web-server'
+  })
+})
+
+server.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    title : 'All about About page',
+    currentYear : new Date().getFullYear()
+  })
+})
+
+server.get('/bad',(req,res)=>{
+  res.send({
+    result : 'error',
+    message : '404!! file not found'
+  })
+})
+
+server.listen(3000)
